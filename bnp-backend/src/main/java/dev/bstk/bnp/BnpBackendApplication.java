2931a4540faf8d21aca337916bb9f97105fc815c
Bruno Luz <bsilva.se@gmail.com>
2024-09-18 20:54:14 -0300

    main - Criando layout frontend

---
+++ bnp-backend/src/main/java/dev/bstk/bnp/BnpBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.bstk.bnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BnpBackendApplication.class, args);
+    }
+
+}
---
+++ bnp-backend/src/test/java/dev/bstk/bnp/BnpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bstk.bnp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
